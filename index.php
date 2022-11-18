f45d0ec23345447903d4ee992302f03070536a4b
bkhorvat <bkhorvat.php@gmail.com>
2022-11-18 21:30:22 +0100

    add routeController, Singleton

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 
 define('VG_ACCESS', true);
 
-header('Content-Type:text/html:charset=utf-8');
+//header('Content-Type:text/html:charset=utf-8');
 session_start();
 
 require_once 'config.php';
 require_once 'core/base/settings/internal_settings.php';
 
 use core\base\exceptions\RouteException;
+use core\base\controllers\RouteController;
 
 try {
-    (new A());
+    RouteController::getInstance()->route();
 } catch (RouteException $e) {
     exit($e->getMessage());
 }

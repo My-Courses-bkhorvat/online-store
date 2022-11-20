6d61384f7484c10d60f454c195c3375685479f69
bkhorvat <bkhorvat.php@gmail.com>
2022-11-20 09:02:25 +0100

    create __construct route controller

--- core/base/controller/RouteController.php
+++ core/base/controller/RouteController.php
@@ -9,6 +9,14 @@ class RouteController
 {
     static private $_instance;
 
+    protected $routes;
+
+    protected $controller;
+    protected $inputMethod;
+    protected $outputMethod;
+    protected $parameters;
+
+
     public function __clone()
     {
     }
@@ -24,9 +32,44 @@ class RouteController
 
     public function __construct()
     {
-        $s = Settings::get('routes');
-        $s1 = ShopSettings::get('routes');
-        exit;
+        $adress_str = $_SERVER['REQUEST_URI'];
+
+        if (strrpos($adress_str, '/') === strlen($adress_str) - 1 && strrpos($adress_str, '/') !== 0) {
+            $this->redirect(rtrim($adress_str, '/'), 301);
+        }
+
+        $path = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php'));
+
+        if ($path === PATH) {
+            $this->routes = Settings::get('routes');
+
+            if (!$this->routes) {
+                throw new \Exception('Сайт знаходиться на технічному обслуговуванні.');
+            }
+
+            if (strrpos($adress_str, $this->routes['admin']['alias'] === strlen(PATH))) {
+
+            } else {
+                $url = explode('/', substr($adress_str, strlen(PATH)));
+
+                $hrUrl = $this->routes['user']['hrUrl'];
+
+                $this->controller = $this->routes['user']['path'];
+
+                $route = 'user';
+            }
+
+            $this->createRoute($route, $url);
+
+            exit();
+        } else {
+            try {
+                throw new \Exception('Не коректна директорія сайту.');
+            } catch (\Exception $e) {
+                exit($e->getMessage());
+            }
+        }
     }
+
 
 }

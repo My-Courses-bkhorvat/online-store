909ea513e876596372b2d66228a9fe976e5fdc21
bkhorvat <bkhorvat.php@gmail.com>
2022-11-24 13:34:28 +0100

    BaseModel create createOrder method

--- core/admin/controller/IndexController.php
+++ core/admin/controller/IndexController.php
@@ -15,14 +15,15 @@ class IndexController extends BaseController
 
         $table = 'category';
 
- /*       $res = $db->get($table, [
+        $res = $db->get($table, [
             'fields' => ['id', 'name'],
             'where' => ['fio' => 'ivanova', 'name' => 'Masha', 'surname' => 'Sergeevna'],
             'operand' => ['=', '<>'],
             'condition' => ['AND'],
-            'order_direction' => ['ASC', 'DESC'],
+            'order' => ['fio', 'name', 'surname'],
+            //'order_direction' => ['ASC', 'DESC'],
             'limit' => '1'
-        ]);*/
+        ]);
 
         exit('I am admin panel.');
     }
